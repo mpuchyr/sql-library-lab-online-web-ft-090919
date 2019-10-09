e0f4615815bf8c825519dd7b427f987d50b01748
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 18:58:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series
-  ON authers.id = series.author_id
+  ON authors.id = series.author_id
   JOIN subgenres
   ON series.subgenre_id = subgenres.id;"
 end
