5132f086fd7e1e1a8f7949df192cd22f213c2214
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 18:49:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,8 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
-  FROM characters;"
+  FROM characters
+  ORDER BY LEN(characters.motto);"
 end
 
 
