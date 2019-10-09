cdc1893380beb16cdd27d9d8992fed760779a34b
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 17:11:37 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,16 +29,5 @@ VALUES ("Jim Butcher"),
   ("George R. R. Martin");
 
 
--- INSERT INTO character_books (character_id, book_id)
--- VALUES (1, 1),
---   (1, 2),
---   (1, 3),
---   (2, 1),
---   (2, 2),
---   (2, 3),
---   (3, 3),
---   (4, 3),
---   (5, 4),
---   (6, 4),
---   (6, 5),
---   (6, 6);
+INSERT INTO character_books (character_id, book_id)
+VALUES (1, 1);
