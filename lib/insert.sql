5a3a20f3c09db40117842b5e7dcac0eeff1f04fb
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 16:47:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,3 +9,10 @@ VALUES ("Storm Front", 2000, 1),
   ("A Game of Thrones", 1996, 2),
   ("A Clash of Kings", 1998, 2),
   ("A Storm of Swords", 2000, 2);
+
+INSERT INTO characters (name, motto, species, author_id)
+VALUES ("Harry Dresden", "Hell's Bells", "human", 1),
+  ("Bob the Skull", "Oh Harry", "spirit", 1),
+  ("Michael Carpenter", "In the name of God", "human", 1),
+  ("Bianca", "Revenge is lik sex...", "vampire", 1),
+  ()
