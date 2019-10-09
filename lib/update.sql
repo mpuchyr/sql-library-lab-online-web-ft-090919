2a2daf0a2c88ecf85372ab8ad1accb455fce7e6b
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 18:43:07 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,8 +1,1 @@
 UPDATE characters SET species = "Martian" WHERE id = 8;
-
-SELECT books.title, books.year
-FROM books
-LEFT OUTER JOIN series
-ON books.series_id = series.id
-GROUP BY series.id
-WHERE series.id = 1;
