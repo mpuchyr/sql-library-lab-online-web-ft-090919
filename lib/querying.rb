808502291214cc6dbf6a01d2a6175d2c9986cd16
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 19:06:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,11 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM series;"
+  FROM series
+  JOIN authors
+  ON series.author_id = authors.id
+  JOIN characters
+  ON authors.id = characters.authors_id;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
