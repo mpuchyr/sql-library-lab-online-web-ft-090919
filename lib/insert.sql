80d6359586fbae2dd57a5a3522da6d3ef62a94c1
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 16:55:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,13 +9,13 @@ VALUES ("Storm Front", 2000, 1),
   ("A Game of Thrones", 1996, 2),
   ("A Clash of Kings", 1998, 2),
   ("A Storm of Swords", 2000, 2);
---
--- INSERT INTO characters (name, motto, species, author_id)
--- VALUES ("Harry Dresden", "Hell's Bells", "human", 1),
---   ("Bob the Skull", "Oh Harry", "spirit", 1),
---   ("Michael Carpenter", "In the name of God", "human", 1),
---   ("Bianca", "Revenge is lik sex...", "vampire", 1),
---   ("Ned Stark", "Winter is coming", "human", 2),
---   ("Tyrion Lannister", "A Lannister always pays his debts", "human", 2)
---   ("Ygritte", "You know nothing, Jon Snow", "human", 2),
---   ("Arya Stark", "Not today", "human", 2);
+
+INSERT INTO characters (name, motto, species, author_id)
+VALUES ("Harry Dresden", "Hell's Bells", "human", 1),
+  ("Bob the Skull", "Oh Harry", "spirit", 1),
+  ("Michael Carpenter", "In the name of God", "human", 1),
+  ("Bianca", "Revenge is lik sex...", "vampire", 1),
+  ("Ned Stark", "Winter is coming", "human", 2),
+  ("Tyrion Lannister", "A Lannister always pays his debts", "human", 2)
+  ("Ygritte", "You know nothing, Jon Snow", "human", 2),
+  ("Arya Stark", "Not today", "human", 2);
