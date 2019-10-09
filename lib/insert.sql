0cf6e1d8f39d6ee87cba078b8c7704121c0eb3c6
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 16:54:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,13 +2,13 @@ INSERT INTO series (title, author, subgenre_id, author_id)
 VALUES ("The Dresden Files", "Jim Butcher", 1, 1),
   ("A Song of Ice and Fire", "George R. R. Martin", 2, 2);
 
--- INSERT INTO books (title, year, series_id)
--- VALUES ("Storm Front", 2000, 1),
---   ("Fool Moon", 2001, 1),
---   ("Grave Peril", 2001, 1),
---   ("A Game of Thrones", 1996, 2),
---   ("A Clash of Kings", 1998, 2),
---   ("A Storm of Swords", 2000, 2);
+INSERT INTO books (title, year, series_id)
+VALUES ("Storm Front", 2000, 1),
+  ("Fool Moon", 2001, 1),
+  ("Grave Peril", 2001, 1),
+  ("A Game of Thrones", 1996, 2),
+  ("A Clash of Kings", 1998, 2),
+  ("A Storm of Swords", 2000, 2);
 --
 -- INSERT INTO characters (name, motto, species, author_id)
 -- VALUES ("Harry Dresden", "Hell's Bells", "human", 1),
