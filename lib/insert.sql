6ffb2e5b3f8c7918476f2c591a08402417265781
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 16:55:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -16,6 +16,6 @@ VALUES ("Harry Dresden", "Hell's Bells", "human", 1),
   ("Michael Carpenter", "In the name of God", "human", 1),
   ("Bianca", "Revenge is lik sex...", "vampire", 1),
   ("Ned Stark", "Winter is coming", "human", 2),
-  ("Tyrion Lannister", "A Lannister always pays his debts", "human", 2)
+  ("Tyrion Lannister", "A Lannister always pays his debts", "human", 2),
   ("Ygritte", "You know nothing, Jon Snow", "human", 2),
   ("Arya Stark", "Not today", "human", 2);
