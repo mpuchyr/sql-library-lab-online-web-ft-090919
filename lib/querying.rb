1c81b399849925db8d14d2bf2f658bf4667385ea
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-09 19:15:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,5 +41,8 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name
   FROM characters
   JOIN character_books
-  ON characters.id = character_books.character_id;"
+  ON characters.id = character_books.character_id
+  JOIN books
+  ON books.id = character_books.book_id
+  GROUP BY books.title;"
 end
